b4d3c0835a6635a4a3bf9473d0729161e6f9c70b
Kiara <kiarabroekhuizen1@hotmail.com>
2022-01-20 20:51:16 +0100

    initial commit

---
+++ src/Detail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Header() {
+
+    return (
+        <div className='Detail'>
+            <header className="header">
+                <ul>
+                    <li><a>Favorieten</a></li>
+                    <li><a>Opslaan</a></li>
+                </ul>
+            </header>
+            <div className="wrapper">
+
+              
+                         
+                
+            </div>
+        </div>
+
+    )
+}
